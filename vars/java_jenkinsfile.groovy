308d554e180e891b64a6afd6a3f5282c21391645
apurbaa10 <43528410+apurbaa10@users.noreply.github.com>
2021-08-19 13:47:47 +0530

    Update java_jenkinsfile.groovy

--- vars/java_jenkinsfile.groovy
+++ vars/java_jenkinsfile.groovy
@@ -1,7 +1,8 @@
 def call(Map specs){
   node('master') {
     stage('Checkout'){
-       println "java specs" + specs
+       println "java specs" + specs.repo
+       println "java specs" + specs.branch
       ciFunc.checkoutVarFunc(specs)
      
     }
