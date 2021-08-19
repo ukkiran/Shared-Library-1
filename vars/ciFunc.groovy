42c58d20ad3b3bffb17f2889ec5235c28a6a4a39
apurbaa10 <43528410+apurbaa10@users.noreply.github.com>
2021-08-19 15:03:24 +0530

    Update ciFunc.groovy

--- vars/ciFunc.groovy
+++ vars/ciFunc.groovy
@@ -1,6 +1,10 @@
 def checkoutVarFunc(Map specs) {
   println "Printing specs" + specs
   docheckout = new com.hexaware.service.Codecheckout(this, specs)
-  docheckout.checkOutFunc(specs)
+  docheckout.checkOutFunc(specs)  
+}
+def build(Map specs) {
+  dobuild = new com.hexaware.service.Build(this, specs)
+  dobuild.buildFunc(specs)
   
 }
