8301e1cf02e711a75f273f1ac0d103d6c014676d
apurbaa10 <43528410+apurbaa10@users.noreply.github.com>
2021-08-20 16:21:21 +0530

    Update java_jenkinsfile.groovy

--- vars/java_jenkinsfile.groovy
+++ vars/java_jenkinsfile.groovy
@@ -18,7 +18,7 @@ def call(){
           }
       }
       catch(Exception e) {
-         println("Exception: ${e}")
+         println("Faisal: ${e}")
         }
     stage('Code Checkout'){
         ciFunc.checkoutVarFunc([
@@ -32,7 +32,7 @@ def call(){
       }
     }
       catch(Exception e) {
-        println("Exception: ${e}")
+        println("Apurba: ${e}")
       }
     }
   }
