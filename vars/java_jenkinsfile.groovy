7d49319569d1d632238a685fc7d698c155435b2d
apurbaa10 <43528410+apurbaa10@users.noreply.github.com>
2021-08-20 12:15:45 +0530

    Update java_jenkinsfile.groovy

--- vars/java_jenkinsfile.groovy
+++ vars/java_jenkinsfile.groovy
@@ -1,6 +1,19 @@
-def call(Map specs){
+def call(){
   node('master') {
-    stage('Checkout'){
+    stage('Specs Checkout'){
+      ciFunc.checkoutVarFunc([
+      repo: Repo
+      branch: Branch
+      ])
+      def specsDir = "./Specs/$Version"
+    
+      if(fileExists(specsDir = "ci_template.yaml")){
+       ci_template = readYaml file : specsDir + "/ci_template.yaml"
+       specs = specs + ci_template
+       println "reading specs file" + specs
+        }
+      }
+    stage('Code Checkout'){
       ciFunc.checkoutVarFunc(specs)
     }
     stage('Build'){
