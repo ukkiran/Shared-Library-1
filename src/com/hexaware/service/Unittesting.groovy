a04cccec8681b481d28b0a26dbf2faa40efffdb1
apurbaa10 <43528410+apurbaa10@users.noreply.github.com>
2021-08-24 16:04:56 +0530

    Update Unittesting.groovy

--- src/com/hexaware/service/Unittesting.groovy
+++ src/com/hexaware/service/Unittesting.groovy
@@ -1,1 +1,15 @@
+package com.hexaware.service
+class Unittesting implements Serializable{
+Script mainScript
+Map specs
+Map config
 
+  def Unittesting(Script mainScript, Map specs, Map config){
+  this.mainScript = mainScript
+  this.specs = specs
+  this.config = config
+  }
+  def UnittestFunc(Map specs, Map config){
+    mainScript.sh config.java.unittest.junit.command 
+  }
+}
