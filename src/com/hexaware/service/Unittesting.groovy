56996c5284bffb438a02f55eece987726b5802e6
apurbaa10 <43528410+apurbaa10@users.noreply.github.com>
2021-08-24 17:12:38 +0530

    Update Unittesting.groovy

--- src/com/hexaware/service/Unittesting.groovy
+++ src/com/hexaware/service/Unittesting.groovy
@@ -9,7 +9,7 @@ Map config
   this.specs = specs
   this.config = config
   }
-  def UnittestFunc(Map specs, Map config){
+  def unitTestFunc(Map specs, Map config){
     mainScript.sh config.java.unittest.junit.command 
   }
 }
