4e5d122a24b93dd8c0410b0059a795497c3ce248
apurbaa10 <43528410+apurbaa10@users.noreply.github.com>
2021-08-24 16:07:14 +0530

    Update Codecoverage.groovy

--- src/com/hexaware/service/Codecoverage.groovy
+++ src/com/hexaware/service/Codecoverage.groovy
@@ -1,1 +1,15 @@
+package com.hexaware.service
+class Codecoverage implements Serializable{
+Script mainScript
+Map specs
+Map config
 
+  def Codecoverage(Script mainScript, Map specs, Map config){
+  this.mainScript = mainScript
+  this.specs = specs
+  this.config = config
+  }
+  def CodecoveragecheckFunc(Map specs, Map config){
+    mainScript.sh config.java.codecoverage.jacoco.command 
+  }
+}
