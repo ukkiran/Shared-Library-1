2ec8eae1761d8f8862c6538e5efa340975634889
apurbaa10 <43528410+apurbaa10@users.noreply.github.com>
2021-08-24 13:48:53 +0530

    Update java_jenkinsfile.groovy

--- vars/java_jenkinsfile.groovy
+++ vars/java_jenkinsfile.groovy
@@ -9,24 +9,25 @@ def call(){
       branch: Branch
       ])
       stage('reading GlobalConfig & Specs'){ 
+        try {
           println "reading the specs from Specs repository"
           def specsDir = "./specs/$Version"
           println "specs version" + specsDir
-          try {
             if(fileExists(specsDir + "/ci_template.yaml")){
              ci_template = readYaml file : specsDir + "/ci_template.yaml"
              specs = specs + ci_template
              println "reading specs file" + specs
               }
-          }
-          catch(Exception e) {
-             println "Error in reading specs file : " + e.getMessage()
-          throw e
+         
+            println "reading the global config from resources"
+            def request = libraryResource "com/hexaware/service/globalConfig/globalConfig.yaml"
+            config = readYaml text: request
+            println "reading config file" + config
             }
-        println "reading the global config from resources"
-        def request = libraryResource "com/hexaware/service/globalConfig/globalConfig.yaml"
-        config = readYaml text: request
-        println "reading config file" + config
+        catch(Exception e) {
+             println "Error in reading specs file : " + e.getMessage()
+        throw e
+            }
       }
      
     stage('Code Checkout'){
