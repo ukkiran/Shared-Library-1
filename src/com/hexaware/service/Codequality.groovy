aef59b022a856dbda465fdd625001971d70a4e61
apurbaa10 <43528410+apurbaa10@users.noreply.github.com>
2021-08-25 18:33:31 +0530

    Update Codequality.groovy

--- src/com/hexaware/service/Codequality.groovy
+++ src/com/hexaware/service/Codequality.groovy
@@ -1,1 +1,16 @@
+package com.hexaware.service
+class Codequality implements Serializable{
+Script mainScript
+Map specs
+Map config
 
+  def Codequality(Script mainScript, Map specs, Map config){
+  this.mainScript = mainScript
+  this.specs = specs
+  this.config = config
+  }
+  def codequalityFunc(Map specs, Map config){
+    mainScript.sh mvn sonar:sonar -Dsonar.projectKey="Petclinic" -Dsonar.host.url="https://sonarcloud.io" -Dsonar.login="74b5ee6411083764b620699a731e7f05f5b12b9b" -Dsonar.projectName="Petclinic"
+     }
+  
+}
