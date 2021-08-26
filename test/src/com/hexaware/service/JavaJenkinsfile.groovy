9f9e97374c8ea03078b9c97ed6bddc36da6c338e
pathanadil <44292489+pathanadil@users.noreply.github.com>
2021-08-26 16:46:49 +0530

    Update JavaJenkinsfile.groovy

--- test/src/com/hexaware/service/JavaJenkinsfile.groovy
+++ test/src/com/hexaware/service/JavaJenkinsfile.groovy
@@ -3,7 +3,7 @@ import com.homeaway.devtools.jenkins.testing.JenkinsPipelineSpecification
 public class JavaJenkinsfile extends JenkinsPipelineSpecification {
 
 	def DefaultPipeline = null
-  def specs = [unitTest[isUnittestRequired: true]]
+  def specs = [unitTest:[isUnittestRequired: true]]
 
 	public static class DummyException extends RuntimeException {
 		public DummyException(String _message) { super( _message ); }
@@ -16,6 +16,6 @@ public class JavaJenkinsfile extends JenkinsPipelineSpecification {
 		when:
 			DefaultPipeline()
 		then:
-			0 * getPipelineMock("ciFunc.unittest.call")()
+			0 * getPipelineMock("ciFunc.unittest.call")(specs)
 	}
 }
