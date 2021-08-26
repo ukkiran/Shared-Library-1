f2684eae329ffee6c9b57fd720cf94c284004aff
apurbaa10 <43528410+apurbaa10@users.noreply.github.com>
2021-08-26 17:26:17 +0530

    Create Uploadartifacts.groovy

---
+++ src/com/hexaware/service/Uploadartifacts.groovy
@@ -0,0 +1,25 @@
+package com.hexaware.service
+class Uploadartifacts implements Serializable{
+Script mainScript
+Map specs
+Map config
+
+  def Uploadartifacts(Script mainScript, Map specs, Map config){
+  this.mainScript = mainScript
+  this.specs = specs
+  this.config = config
+  }
+  def uploadartifactFunc(Map specs, Map config){
+      def server = mainScript.Artifactory.server 'artifactory'
+        def uploadSpec = """{
+        "files": [
+          {
+            "pattern": "target/*.jar",
+            "target": "default-maven-local/petclinic/jar/"
+          }
+       ]
+      }"""
+    def buildInfo = server.upload(uploadSpec)  
+    mainScript.server.publishBuildInfo(buildInfo)
+  }
+}
