9907d2f7731262bfc9e25c828bd152b8f426dec6
apurbaa10 <43528410+apurbaa10@users.noreply.github.com>
2021-08-27 15:15:31 +0530

    Update Dockerdeploy.groovy

--- src/com/hexaware/service/Dockerdeploy.groovy
+++ src/com/hexaware/service/Dockerdeploy.groovy
@@ -11,7 +11,7 @@ Map config
   }
   def dockerdeployFunc(Map specs, Map config){
     
-      mainScript.sh """ docker run -d -name petclinic apurbaa10.jfrog.io/default-docker-local/petclinic:V1 """
+      mainScript.sh """ docker run -d --name petclinic apurbaa10.jfrog.io/default-docker-local/petclinic:V1 """
     
   }
 }
