0e60cc64544e6b995335f7f1d8d2a5e015024be2
ukkiran <65678960+ukkiran@users.noreply.github.com>
2021-10-05 13:12:42 +0530

    Update Dockerdeploy.groovy

--- src/com/hexaware/service/Dockerdeploy.groovy
+++ src/com/hexaware/service/Dockerdeploy.groovy
@@ -11,8 +11,7 @@ Map config
   }
   def dockerdeployFunc(Map specs, Map config){
      
-      mainScript.sh """docker pull ukkiran23.jfrog.io/default-docker-local/petclinic:V1
-      docker run -d -p 8085:8085 --name petclinic https://ukkiran23.jfrog.io/artifactory/default-docker-local/petclinic:V1 """
+      mainScript.sh """docker run -d -p 8085:8085 --name petclinic ukkiran23.jfrog.io/default-docker-local/petclinic:V1 """
     
   }
 }
